160c263bf68dffe56bedd206158af388a2c19db7
unknown <kilberty1995@gmail.com>
2024-06-30 19:29:18 -0300

    tela torradeira

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,7 +72,7 @@
     function checkScreenSize() {
       const width = window.innerWidth;
       const height = window.innerHeight;
-      if ((width === 800 && height >= 420 )
+      if ((width === 800 && height === 420 )
      ) {
       
         document.body.classList.remove('hidden-content');
